eb4b5b72325441ea411d7b8ab57dc9d0f910b5fd
sudhanshu-mykaarma <sudhanshu.tripathi@mykaarma.com>
2024-11-10 20:26:04 +0530

    hey there, its a angular todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Item } from "./item";
+import { ItemComponent } from "./item/item.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ItemComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
+
 export class AppComponent {
-  title = 'todo';
+
+  componentTitle = "My To Do List";
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems
+    }
+    return this.allItems.filter((item) => this.filter === 'done' ? item.done : !item.done)
+  }
+
+  addItem(description: string) {
+    if (!description) return;
+
+    this.allItems.unshift({
+      description, done: false
+    })
+  }
+
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+
+
 }
